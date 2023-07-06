27ec81b18bf4d8a26275701aa8a2a730b79572b2
atpoint <atpoint90@gmail.com>
2023-07-06 13:19:49 +0200

    check for mismatch between tx2gene and quant.sf

--- bin/tximport.R
+++ bin/tximport.R
@@ -13,10 +13,37 @@ q        <- paste0(quants, "/quant.sf")
 names(q) <- bname
 q        <- q[order(names(q))]
 
+# Add a simple check if there is a mismatch between quant.sf and tx2gene that can be solved
+# be removing txversion or bar
+q1 <- read.delim(q[1], header=TRUE)
+v <- q1[,1,drop=TRUE]
+as.is <- v %in% tx2gene[,1,drop=TRUE] 
+no.dot <- gsub("\\..*", "", v) %in% tx2gene[,1,drop=TRUE] 
+no.bar <- gsub("\\|.*", "", v) %in% tx2gene[,1,drop=TRUE] 
+l <- c(sum(as.is), sum(no.dot), sum(no.bar))
+w <- which(l==max(l))
+
+if(1 %in% w){
+  ignoreTxVersion <- FALSE
+  ignoreAfterBar  <- FALSE
+}
+
+if(2 %in% w & length(w)==1){
+  ignoreTxVersion <- TRUE
+  ignoreAfterBar  <- FALSE
+}
+
+if(3 %in% w & length(w)==1){
+  ignoreTxVersion <- FALSE
+  ignoreAfterBar  <- TRUE
+}
+
 txi <- tximport::tximport(files=q, tx2gene=tx2gene, 
                           type="salmon", importer=read.delim,
                           countsFromAbundance="lengthScaledTPM",
-                          dropInfReps=TRUE)
+                          dropInfReps=TRUE, 
+                          ignoreTxVersion=ignoreTxVersion, 
+                          ignoreAfterBar=ignoreAfterBar)
 
 #/ save counts with length being the median of average tx length:
 tgz <- gzfile("counts.txt.gz", "w")
@@ -28,5 +55,3 @@ tgz <- gzfile("lengths.txt.gz", "w")
 write.table(x=data.frame(gene=rownames(txi$length), txi$counts), 
             file=tgz, sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE)
 close(tgz)
-                     
-                     
